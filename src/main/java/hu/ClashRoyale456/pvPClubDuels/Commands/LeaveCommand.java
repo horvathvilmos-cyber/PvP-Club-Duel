8f3b1aae1ce963c84652bcde9709d9e0bf184c3f
horvathvilmos-cyber <horvath.vilmos@sztorsolya.hu>
2026-05-09 09:01:48 +0200

    Implement player check in LeaveCommand
    Added player type check and error message for LeaveCommand.

--- src/main/java/hu/ClashRoyale456/pvPClubDuels/Commands/LeaveCommand.java
+++ src/main/java/hu/ClashRoyale456/pvPClubDuels/Commands/LeaveCommand.java
@@ -8,6 +8,9 @@ import org.jetbrains.annotations.NotNull;
 public class LeaveCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
+        if (!(commandSender instanceof Player bukkitPlayer)) {
+            commandSender.sendMessage(ChatColor.RED + "You must be a player to use this command.");
+        }
         return false;
     }
 }

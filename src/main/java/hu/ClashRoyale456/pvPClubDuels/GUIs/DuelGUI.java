0c31fb4a125cafe7d7300a69a86dd5787a3feae3
horvathvilmos-cyber <horvath.vilmos@sztorsolya.hu>
2026-05-26 18:58:04 +0200

    Add files via upload

--- src/main/java/hu/ClashRoyale456/pvPClubDuels/GUIs/DuelGUI.java
+++ src/main/java/hu/ClashRoyale456/pvPClubDuels/GUIs/DuelGUI.java
@@ -10,6 +10,24 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class DuelGUI {
-  Inventory inv = Bukkit.createInventory(null, 27, Component.text("Duel with %player%"));
 
+    public static Inventory create() {
+        Inventory inv = Bukkit.createInventory(null, 27, Component.text("Duel Modes"));
+
+        inv.setItem(10, createItem(Material.IRON_SWORD, "Sword"));
+        inv.setItem(12, createItem(Material.IRON_AXE, "Axe"));
+        inv.setItem(14, createItem(Material.POTION, "Pot"));
+        inv.setItem(16, createItem(Material.SPLASH_POTION, "NethPot"));
+        inv.setItem(22, createItem(Material.DIAMOND, "SMP"));
+
+        return inv;
+    }
+
+    private static ItemStack createItem(Material mat, String name) {
+        ItemStack item = new ItemStack(mat);
+        ItemMeta meta = item.getItemMeta();
+        meta.displayName(Component.text(name));
+        item.setItemMeta(meta);
+        return item;
+    }
 }

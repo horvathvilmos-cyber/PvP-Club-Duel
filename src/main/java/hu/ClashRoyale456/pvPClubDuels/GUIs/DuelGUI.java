639ca3b96e88e2a622719750253e03783a3c66c3
horvathvilmos-cyber <horvath.vilmos@sztorsolya.hu>
2026-05-26 15:33:56 +0200

    Implement inventory setup in DuelGUI
    Added inventory initialization for DuelGUI.

--- src/main/java/hu/ClashRoyale456/pvPClubDuels/GUIs/DuelGUI.java
+++ src/main/java/hu/ClashRoyale456/pvPClubDuels/GUIs/DuelGUI.java
@@ -1,4 +1,15 @@
 package hu.ClashRoyale456.pvPClubDuels.GUIs;
 
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Item;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.meta.ItemMeta;
+
 public class DuelGUI {
+  Inventory inv = Bukkit.createInventory(null, 27, Component.text("teszt menü"));
+
 }
